cbf8b4b8bebeb9f028aaaa1eed95d7be68dc8f79
Hunter Johnson <hjohns11@macalester.edu>
2013-10-01 19:41:23 +0000

    Random junk, cleaning out GitHub folder

---
+++ Day-03/Day 03.R
@@ -0,0 +1,6 @@
+# Strings, Numbers, and collections
+
+Funny things happen when you construct things
+* The quotes are a signal to the interpreter, not part of the string itself
+* To put in quotes, escape them
+* Things like '\n' indicate "control characters"
