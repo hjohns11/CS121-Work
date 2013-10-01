042de820b5e2e60bde759656733e14d6f1b628d7
Hunter Johnson <hjohns11@macalester.edu>
2013-10-01 19:38:30 +0000

    Old documents with updates

--- Circles and squares.R
+++
@@ -1,7 +0,0 @@
-plot(1:2, ylab = "y-axis",type="n", xlim = c(0,100), ylim=c(0,100))
-
-polygon(c(25,35,35,25), c(25,25,35,35),angle=90,col="red")
-
-polygon(c(15,35,35,15), c(45,45,70,70),angle=90,col="green", border="blue")
-
-polygon(c(65,80,85,72.5,60), c(50,50,70,80,70),angle=75,col="yellow")
